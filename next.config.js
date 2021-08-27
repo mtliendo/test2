bd8141185ffc9e7eac2bd74c875d3c2d6650f842
Michael Liendo <mtliendo@amazon.com>
2021-08-26 21:30:09 -0500

    udpate with env vars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  target: 'serverless',
   reactStrictMode: true,
   images: {
     domains: ['user-images.githubusercontent.com', 'images.unsplash.com'],
+  },
+  env: {
+    GITHUB_REPO: process.env.GITHUB_REPO,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME,
   },
 }
